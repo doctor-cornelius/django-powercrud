178d42633ba0ea207c3d31796f70055a78696478
Michael Foulds <mfoulds@navigatus.net>
2025-02-16 00:15:16 +1100

    feat(filter): 300ms delay works for text filter

--- sample/filters.py
+++ sample/filters.py
@@ -1,5 +1,5 @@
 from django import forms
-from django_filters import FilterSet
+from django_filters import FilterSet, CharFilter
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Row, Column
 
@@ -9,28 +9,29 @@ from crispy_forms import bootstrap, layout
 from .models import Book
 
 class BookFilterSet(FilterSet):
+    title = CharFilter(lookup_expr='icontains') 
     class Meta:
         model = Book
-        fields = ['author', 'title']
+        fields = ['author', 'title', 'published_date']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         print("DEBUG: Fields after super:", hasattr(self, 'fields'))
         
         # Now we can access self.fields
-        # for field in self.fields.values():
-        #     if isinstance(field.widget, forms.TextInput):
-        #         field.widget.attrs.update({
-        #             'hx-get': '',
-        #             'hx-trigger': 'keyup changed delay:300ms',
-        #             'hx-target': '#filter-results'
-        #         })
-        #     else:
-        #         field.widget.attrs.update({
-        #             'hx-get': '',
-        #             'hx-trigger': 'change',
-        #             'hx-target': '#filter-results'
-        #         })
+        for field in self.form.fields.values():
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'hx-get': '',
+                    'hx-trigger': 'keyup changed delay:300ms',
+                    'hx-target': '#content'
+                })
+            else:
+                field.widget.attrs.update({
+                    'hx-get': '',
+                    'hx-trigger': 'change',
+                    'hx-target': '#content'
+                })
 
         # Set up crispy form helper after field modifications
         self.helper = FormHelper()
