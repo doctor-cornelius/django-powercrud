c3f3b562779389fda1abfa9ddb86ee5654d0b602
Michael Foulds <mfoulds@navigatus.net>
2025-02-16 00:04:49 +1100

    wip(filter): filter working with htmx but need 300ms for text; also need htmx conditional else button

--- sample/filters.py
+++ sample/filters.py
@@ -1,3 +1,4 @@
+from django import forms
 from django_filters import FilterSet
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Row, Column
@@ -14,8 +15,26 @@ class BookFilterSet(FilterSet):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        print("DEBUG: Fields after super:", hasattr(self, 'fields'))
+        
+        # Now we can access self.fields
+        # for field in self.fields.values():
+        #     if isinstance(field.widget, forms.TextInput):
+        #         field.widget.attrs.update({
+        #             'hx-get': '',
+        #             'hx-trigger': 'keyup changed delay:300ms',
+        #             'hx-target': '#filter-results'
+        #         })
+        #     else:
+        #         field.widget.attrs.update({
+        #             'hx-get': '',
+        #             'hx-trigger': 'change',
+        #             'hx-target': '#filter-results'
+        #         })
+
+        # Set up crispy form helper after field modifications
         self.helper = FormHelper()
         self.helper.form_tag = False
         self.helper.disable_csrf = True
-        self.helper.wrapper_class = 'col-auto mb-0'  # Override the margin-bottom
+        self.helper.wrapper_class = 'col-auto'
         self.helper.template = 'bootstrap5/layout/inline_field.html'
