d0b7341a7bae92e0a1ba75d997cfb54cb9720b17
Michael Foulds <mfoulds@navigatus.net>
2025-04-02 16:14:12 +1100

    feat(nm_extract_tailwind_classes): create management command to extract classes for downstream to include in safelist

--- new_release.sh
+++ new_release.sh
@@ -47,6 +47,8 @@ cz changelog --unreleased-version=$NEW_VERSION
 # Build CSS for production
 # NODE_ENV=production npx tailwindcss -i django_nominopolitan/static/css/input.css -o django_nominopolitan/static/css/output.css --minify
 npx webpack --mode production
+# update the safelist
+./manage.py nm_extract_tailwind_classes
 
 # Note scope (release) is what will trigger the release job
 git add -A
